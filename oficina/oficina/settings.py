7c0cee0557c0eef67bc02c938205acc19e6beff6
Brenno <brennoxlr8@gmail.com>
2024-11-11 22:21:14 -0400

    mensagem

--- oficina/oficina/settings.py
+++ oficina/oficina/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'fornecedor',
     'ordem_servico',
     'veiculo',
-    
 ]
 
 MIDDLEWARE = [
--- oficina/oficina/urls.py
+++ oficina/oficina/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings 
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", TemplateView.as_view(template_name="base_tw.html"), name="home")
 ]
 if settings.DEBUG: urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
